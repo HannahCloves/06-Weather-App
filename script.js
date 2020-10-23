e9c13257ee7180751343e1a138043e58bda555ec
Hannah C <hannah.cloves@outlook.com>
2020-10-23 16:57:58 +0100

    updated colorscheme in CSS

--- script.js
+++ script.js
@@ -7,10 +7,23 @@ $(document).ready(function () {
     $("#dayFiveHeader").append(moment().add(4, 'days').format("ll"));
 
 
-
-
-
-
-
-
 });
+
+
+const locationSearch = $("#location-search");
+
+locationSearch.search(function (event) {
+  event.preventDefault();
+
+  const queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + movieTitleString + "&appid=b3b7b9cfe416e5f453d88191c003cae5";
+
+  $.ajax({
+    url: queryURL
+  }).then(function (weatherData) {
+    updateResults(weatherData);
+
+  }).catch(function (error) {
+    console.log("ERROR");
+  })
+});
+
