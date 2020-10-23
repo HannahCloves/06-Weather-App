febae202a7b4102312df0ac24eab38ca11a46430
Hannah C <hannah.cloves@outlook.com>
2020-10-23 18:42:33 +0100

    temperature is showing in console log

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 $(document).ready(function () {
 
-    $("#dayOneHeader").append(moment().format("ll"));
-    $("#dayTwoHeader").append(moment().add(1, 'days').format("ll"));
-    $("#dayThreeHeader").append(moment().add(2, 'days').format("ll"));
-    $("#dayFourHeader").append(moment().add(3, 'days').format("ll"));
-    $("#dayFiveHeader").append(moment().add(4, 'days').format("ll"));
+    const todaysDate = $("#mainLocation").append(moment().format("ll"));
+    $("#dayOneHeader").append(moment().add(1, 'days').format("ll"));
+    $("#dayTwoHeader").append(moment().add(2, 'days').format("ll"));
+    $("#dayThreeHeader").append(moment().add(3, 'days').format("ll"));
+    $("#dayFourHeader").append(moment().add(4, 'days').format("ll"));
+    $("#dayFiveHeader").append(moment().add(5, 'days').format("ll"));
 
 
 });
@@ -14,17 +15,21 @@ const locationSearch = $("#location-search");
 const locationPlace = $("#location-search input[name='search-location']");
 
 locationSearch.submit(function (event) {
-    event.preventDefault();
     const locationPlaceString = locationPlace.val()
-    const queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + locationPlaceString + "&appid=b3b7b9cfe416e5f453d88191c003cae5";
-    console.log(queryURL);
+    event.preventDefault();
+    const queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + locationPlaceString + "&appid=b3b7b9cfe416e5f453d88191c003cae5";
     
-
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function(locationData){
+        updateMainResults(locationData);
+    });
+    
+    function updateMainResults(locationData) {
+        const locationTemp = locationData.main.temp
+    console.log(locationTemp)
+    }
 });
 
-function updateResults(weatherData) {
 
-
-
-
-}
